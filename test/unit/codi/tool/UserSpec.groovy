fa05200504067af1ce321dc9dd68869c4a6e8827
Tomasz Janek <janek.tomasz@gmail.com>
2014-06-22 09:56:08 +0200

    Add user domain model specification - refactor

--- test/unit/codi/tool/UserSpec.groovy
+++ test/unit/codi/tool/UserSpec.groovy
@@ -2,73 +2,31 @@ package codi.tool
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 @TestFor(User)
 class UserSpec extends Specification {
 
-    void "Test email for user"() {
-        when: 'email is blank'
-        def user = new User(email: '')
+    @Unroll('User #field with value #value should get be validated #validate')
+    def 'User should be validated'() {
+        when:
+        def user = new User("$field": value)
 
-        then: 'validation should fail'
-        !user.validate(['email'])
+        then:
+        user.validate([field]) == validate
 
-        when: 'email is not set'
-        user = new User()
-
-        then: 'validation should fail'
-        !user.validate(['email'])
-
-        when: 'invalid email is set'
-        user = new User(email: 'test')
-
-        then: 'validation should fail'
-        !user.validate(['email'])
-
-        when: 'valid email is set'
-        user = new User(email: 'test@test.pl')
-
-        then: 'validation should pass'
-        user.validate(['email'])
+        where:
+        field      | value           | validate
+        'email'    | ''              | false
+        'email'    | null            | false
+        'email'    | 'test'          | false
+        'email'    | 'test@user.com' | true
+        'password' | ''              | false
+        'password' | null            | false
+        'password' | 'pass'          | false
+        'password' | 'password'      | true
+        'name'     | null            | false
+        'name'     | 'Some user'     | true
     }
 
-    void "Test password for user"() {
-        when: 'password is blank'
-        def user = new User(password: '')
-
-        then: 'validation should fail'
-        !user.validate(['password'])
-
-        when: 'password is not set'
-        user = new User()
-
-        then: 'validation should fail'
-        !user.validate(['password'])
-
-        when: 'password has wrong size'
-        user = new User(password: 'pass')
-
-        then: 'validation should fail'
-        !user.validate(['password'])
-
-        when: 'valid password is set'
-        user = new User(password: 'password')
-
-        then: 'validation should pass'
-        user.validate(['password'])
-    }
-
-    void "Test username for user"() {
-        when: 'username is not set'
-        def user = new User()
-
-        then: 'validation should fail'
-        !user.validate(['name'])
-
-        when: 'username is set'
-        user = new User(name: "Some user")
-
-        then: 'validation should pass'
-        user.validate(['name'])
-    }
 }
