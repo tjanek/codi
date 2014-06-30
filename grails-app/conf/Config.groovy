87408b1f1b418cf8a5351236093830be5081a302
Tomasz Janek <janek.tomasz@gmail.com>
2014-06-30 20:32:50 +0200

    Logging for grails jobs

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -114,4 +114,7 @@ log4j.main = {
            'org.springframework',
            'org.hibernate',
            'net.sf.ehcache.hibernate'
+           'grails.app.jobs'
+
+    debug 'grails.app.jobs'
 }
