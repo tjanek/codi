9839c6962354f88f5bf57eb8586dd169177ba8a2
tjanek <janek.tomasz@gmail.com>
2014-07-04 11:12:24 +0200

    Add twitter boostrap plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -70,6 +70,8 @@ grails.project.dependency.resolution = {
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
 
+        runtime ':twitter-bootstrap:3.2.0'
+
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.7.4"
         //compile ":less-asset-pipeline:1.7.0"
